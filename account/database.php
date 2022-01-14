10257b3e36ac1d39520fd62afc15185b40620196
GODGEARHD <godgearhd22@gmail.com>
2022-01-14 13:14:57 +0100

    PROYECTO TFG DAM

--- account/database.php
+++ account/database.php
@@ -2,7 +2,7 @@
 
 $server = 'localhost:3306';
 $username = 'root';
-$password = password_verify('/root/php', PASSWORD_BCRYPT);
+$password = password_verify('/root/php2', '/root/php');
 $database = 'login';
 
 try {
