778303465d4cdc66244ff7e65a60984ceb98a2e3
GODGEARHD <godgearhd22@gmail.com>
2022-01-14 13:56:09 +0100

    PROYECTO TFG DAM

--- start.php
+++ start.php
@@ -20,6 +20,8 @@
 
     require 'database.php';
 
+    $message = '';
+
     if (!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['telefono']) && !empty($_POST['resumen'])) {
         $sql = "INSERT INTO feedback (name, email, telephone, opinion) VALUES (:nombre, :email, :telefono, :resumen)";
         $stmt = $conn->prepare($sql);
@@ -30,10 +32,30 @@
 
         if ($stmt->execute()) {
             $message = 'Thanks for your feedback!';
-            header('Location: ./index');
+            header('Location: ./start');
         } else {
             $message = 'Sorry, there must have been an issue while saving your comments';
         }
 }
 
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to you WebApp</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <?php require 'partials/header.php' ?>
+      <br> <?= $message ?>
+      <br>You are Successfully Logged In
+      <br>
+      <br>
+      <form action="../index" method="POST">
+        <input type="submit" value="Go Home">
+      </form>
+  </body>
+</html>
