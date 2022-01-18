5061a3187b4fa3114d2ab5150c0d1e79efc1ed43
GODGEARHD <godgearhd22@gmail.com>
2022-01-18 15:04:10 +0100

    PROYECTO TFG DAM

---
+++ account/customize.php
@@ -0,0 +1,43 @@
+<?php
+
+  error_reporting(0);
+
+    require 'database.php';
+
+    $message = '';
+
+    if (!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['telefono']) && !empty($_POST['resumen'])) {
+        $sql = "INSERT INTO custom_cfg (name, email, telephone, opinion) VALUES (:nombre, :email, :telefono, :resumen)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':nombre', $_POST['nombre']);
+        $stmt->bindParam(':email', $_POST['email']);
+        $stmt->bindParam(':telefono', $_POST['telefono']);
+        $stmt->bindParam(':resumen', $_POST['resumen']);
+
+        if ($stmt->execute()) {
+            $message = '¡Gracias por tus comentarios!';
+        } else {
+            $message = 'Lo sentimos, se ha debido de producir algun error al procesar tus comentarios';
+        }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>¡Gracias!</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <?php require 'partials/header.php' ?>
+      <br> <?= $message ?>
+      <br>
+      <br>
+      <form action="../" method="POST">
+        <input type="submit" value="Ir a Inicio">
+      </form>
+  </body>
+</html>
