9bcad2c49205b1b6dd231fe849b1ce541df94371
GODGEARHD <godgearhd22@gmail.com>
2022-04-01 12:46:23 +0200

    PROYECTO TFG DAM

--- account/customize.php
+++ account/customize.php
@@ -21,14 +21,32 @@
     $message = '';
 
     if (!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['paypal'])) {
-        $sql = "INSERT INTO custom_cfg (nombre_usuario, email, paypal, precio_total, cpu, refri_cpu, ram, placa_base, disco_duro_1, disco_duro_2, caja, psu, gpu, tarjeta_sonido, lector_tarjetas, dvd, monitor, teclado, raton, altavoces, tarjeta_tv, adaptador_eth_wifi, montaje, sistema_operativo) 
-                                VALUES (:nombre, :email, :paypal, :resumen)";
+        $sql = "INSERT INTO custom_cfg (nombre_usuario, email, paypal, precio_total, cpu, refri_cpu, ram, placa_base, disco_duro_1, disco_duro_2, caja, psu, gpu, tarjeta_sonido, lector_tarjetas, dvd, monitor, teclado, raton, altavoces, tarjeta_tv, adaptador_eth_wifi, montaje, sistema_operativo) VALUES (:nombre, :email, :paypal, :precio, :cpu, :refri, :ram, :placa, :disco1, :disco2, :caja, :psu, :gpu, :audio, :lector, :dvd, :monitor, :teclado, :raton, :altavoces, :tv, :red, :montaje, :so)";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':nombre', $_POST['nombre']);
         $stmt->bindParam(':email', $_POST['email']);
         $stmt->bindParam(':paypal', $_POST['paypal']);
-        $stmt->bindParam(':resumen', $_POST['resumen']);
-        $stmt->bindParam(':resumen', $_POST['resumen']);
+        $stmt->bindParam(':precio', $_POST['precio']);
+        $stmt->bindParam(':cpu', $_POST['cpu']);
+        $stmt->bindParam(':refri', $_POST['refri']);
+        $stmt->bindParam(':ram', $_POST['ram']);
+        $stmt->bindParam(':placa', $_POST['placa']);
+        $stmt->bindParam(':disco1', $_POST['disco1']);
+        $stmt->bindParam(':disco2', $_POST['disco2']);
+        $stmt->bindParam(':caja', $_POST['caja']);
+        $stmt->bindParam(':psu', $_POST['psu']);
+        $stmt->bindParam(':gpu', $_POST['gpu']);
+        $stmt->bindParam(':audio', $_POST['audio']);
+        $stmt->bindParam(':lector', $_POST['lector']);
+        $stmt->bindParam(':dvd', $_POST['dvd']);
+        $stmt->bindParam(':monitor', $_POST['monitor']);
+        $stmt->bindParam(':teclado', $_POST['teclado']);
+        $stmt->bindParam(':raton', $_POST['raton']);
+        $stmt->bindParam(':altavoces', $_POST['altavoces']);
+        $stmt->bindParam(':tv', $_POST['tv']);
+        $stmt->bindParam(':red', $_POST['red']);
+        $stmt->bindParam(':montaje', $montaje);
+        $stmt->bindParam(':so', $_POST['so']);
 
         if ($stmt->execute()) {
             $message = '¡Gracias por tus comentarios!';
