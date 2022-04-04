c302b29d383e30ef0f3663a4ba56d7b1ff522f03
GODGEARHD <godgearhd22@gmail.com>
2022-04-04 16:11:38 +0200

    PROYECTO TFG DAM

--- script/customizerImg.js
+++ script/customizerImg.js
@@ -1,3 +1,75 @@
-function test() {
+function cpu() {
+    console.log("works")
+}
+
+function refri() {
+    console.log("works")
+}
+
+function ram() {
+    console.log("works")
+}
+
+function placa() {
+    console.log("works")
+}
+
+function disco1() {
+    console.log("works")
+}
+
+function disco2() {
+    console.log("works")
+}
+
+function caja() {
+    console.log("works")
+}
+
+function psu() {
+    console.log("works")
+}
+
+function gpu() {
+    console.log("works")
+}
+
+function audio() {
+    console.log("works")
+}
+
+function lector() {
+    console.log("works")
+}
+
+function dvd() {
+    console.log("works")
+}
+
+function monitor() {
+    console.log("works")
+}
+
+function teclado() {
+    console.log("works")
+}
+
+function mouse() {
+    console.log("works")
+}
+
+function altavoces() {
+    console.log("works")
+}
+
+function tv() {
+    console.log("works")
+}
+
+function red() {
+    console.log("works")
+}
+
+function so() {
     console.log("works")
 }
