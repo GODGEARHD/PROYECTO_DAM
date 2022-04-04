2354e0e3945204959a6e2d6e91ec4b967decbe56
GODGEARHD <godgearhd22@gmail.com>
2022-04-04 16:15:27 +0200

    PROYECTO TFG DAM

--- script/customizerImg.js
+++ script/customizerImg.js
@@ -1,75 +1,75 @@
 function cpu() {
-    console.log("works")
+    console.log("works");
 }
 
 function refri() {
-    console.log("works")
+    console.log("works");
 }
 
 function ram() {
-    console.log("works")
+    console.log("works");
 }
 
 function placa() {
-    console.log("works")
+    console.log("works");
 }
 
 function disco1() {
-    console.log("works")
+    console.log("works");
 }
 
 function disco2() {
-    console.log("works")
+    console.log("works");
 }
 
 function caja() {
-    console.log("works")
+    console.log("works");
 }
 
 function psu() {
-    console.log("works")
+    console.log("works");
 }
 
 function gpu() {
-    console.log("works")
+    console.log("works");
 }
 
 function audio() {
-    console.log("works")
+    console.log("works");
 }
 
 function lector() {
-    console.log("works")
+    console.log("works");
 }
 
 function dvd() {
-    console.log("works")
+    console.log("works");
 }
 
 function monitor() {
-    console.log("works")
+    console.log("works");
 }
 
 function teclado() {
-    console.log("works")
+    console.log("works");
 }
 
 function mouse() {
-    console.log("works")
+    console.log("works");
 }
 
 function altavoces() {
-    console.log("works")
+    console.log("works");
 }
 
 function tv() {
-    console.log("works")
+    console.log("works");
 }
 
 function red() {
-    console.log("works")
+    console.log("works");
 }
 
 function so() {
-    console.log("works")
+    console.log("works");
 }
