f396304ec06c474cf102a6739b2caaab73577ebf
GODGEARHD <godgearhd22@gmail.com>
2022-04-06 13:41:58 +0200

    PROYECTO TFG DAM

--- script/ses.js
+++ script/ses.js
@@ -1,7 +1,11 @@
-if ((document.cookie.match(/^(.*;)?\s*PHPSESSID\s*=\s*[^;]+(.*)?$/)) != null) {
-    document.getElementById("sesion1").innerHTML = "¡Bienvenid@ de vuelta!";
-    document.getElementById("sesion1").href = "#";
-    document.getElementById("sesion1").classList.add("disabled");
-    document.getElementById("sesion2").innerHTML = "Cerrar Sesión";
-    document.getElementById("sesion2").href = "/account/logout";
+ses();
+
+function ses() {
+    if ((document.cookie.match(/^(.*;)?\s*PHPSESSID\s*=\s*[^;]+(.*)?$/)) != null) {
+        document.getElementById("sesion1").innerHTML = "¡Bienvenid@ de vuelta!";
+        document.getElementById("sesion1").href = "#";
+        document.getElementById("sesion1").classList.add("disabled");
+        document.getElementById("sesion2").innerHTML = "Cerrar Sesión";
+        document.getElementById("sesion2").href = "/account/logout";
+    }
 }
