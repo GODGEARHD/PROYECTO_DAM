7880e0e8b9bcfc577f3230fc6239d7c91d7a4816
GODGEARHD <godgearhd22@gmail.com>
2022-04-06 11:02:01 +0200

    PROYECTO TFG DAM

--- account/signup.php
+++ account/signup.php
@@ -1,66 +1,67 @@
 <?php
 
-error_reporting(0);
+error_reporting( 0 );
 
 require 'database.php';
 
 $message = '';
 
-if (!empty($_POST['email']) && !empty($_POST['password'])) {
-  $records = $conn->prepare('SELECT id, email, password FROM users WHERE email = :email');
-  $records->bindParam(':email', $_POST['email']);
-  $records->execute();
-  $results = $records->fetch(PDO::FETCH_ASSOC);
+if ( !empty( $_POST[ 'email' ] ) && !empty( $_POST[ 'password' ] ) ) {
+    $records = $conn->prepare( 'SELECT id, email, password FROM users WHERE email = :email' );
+    $records->bindParam( ':email', $_POST[ 'email' ] );
+    $records->execute();
+    $results = $records->fetch( PDO::FETCH_ASSOC );
 
-  $message = '';
-  $message2 = '';
+    $message = '';
+    $message2 = '';
 
-  if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
-    $message = '¡Vaya, parece que ya existe un usuario con ese email! ¿Por qué no intentas ';
-    $message2 = 'iniciar sesión?';
-  } else {
-    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':email', $_POST['email']);
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-    $stmt->bindParam(':password', $password);
-
-    if ($stmt->execute()) {
-      $message = 'Successfully created new user';
-      header('Location: ./login');
+    if ( count( $results ) > 0 && password_verify( $_POST[ 'password' ], $results[ 'password' ] ) ) {
+        $message = '¡Vaya, parece que ya existe un usuario con ese email! ¿Por qué no intentas ';
+        $message2 = 'iniciar sesión?';
     } else {
-      $message = 'Sorry there must have been an issue creating your account';
+        $sql = 'INSERT INTO users (email, password) VALUES (:email, :password)';
+        $stmt = $conn->prepare( $sql );
+        $stmt->bindParam( ':email', $_POST[ 'email' ] );
+        $password = password_hash( $_POST[ 'password' ], PASSWORD_BCRYPT );
+        $stmt->bindParam( ':password', $password );
+
+        if ( $stmt->execute() ) {
+            $message = 'Nuevo usuario creado correctamente';
+            header( 'Location: ./login' );
+        } else {
+            $message = 'Lo sentimos, ha debido producirse algún error mientras creabamos tu cuenta.';
+        }
     }
-  }
 }
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <title>SignUp</title>
-  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-  <link rel="stylesheet" href="assets/css/style.css">
+<meta charset = 'utf-8'>
+<title>Registro</title>
+<link href = 'https://fonts.googleapis.com/css?family=Roboto' rel = 'stylesheet'>
+<link rel = 'stylesheet' href = 'assets/css/style.css'>
 </head>
 
 <body>
 
-  <?php require 'partials/header.php' ?>
+<?php require 'partials/header.php' ?>
 
-  <?php if (!empty($message)) : ?>
-    <p> <?= $message ?><a href="login"> <?= $message2 ?></a></p>
-  <?php endif; ?>
+<?php if ( !empty( $message ) ) : ?>
+<p> < ?= $message ?><a href = 'login'> < ?= $message2 ?></a></p>
+<?php endif;
+?>
 
-  <h1>SignUp</h1>
-  <span>or <a href="login">Login</a></span>
+<h1>Registrarse</h1>
+<span>o <a href = 'login'>Iniciar Sesión</a></span>
 
-  <form action="signup" method="POST">
-    <input name="email" type="email" placeholder="Enter your email">
-    <input name="password" type="password" placeholder="Enter your Password">
-    <input name="confirm_password" type="password" placeholder="Confirm Password">
-    <input type="submit" value="Submit">
-  </form>
+<form action = 'signup' method = 'POST'>
+<input name = 'email' type = 'email' placeholder = 'Introduce tu email'>
+<input name = 'password' type = 'password' placeholder = 'Introduce tu contraseña'>
+<input name = 'confirm_password' type = 'password' placeholder = 'Confirmar contraseña'>
+<input type = 'submit' value = 'Enviar'>
+</form>
 
 </body>
 
