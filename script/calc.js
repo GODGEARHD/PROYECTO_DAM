b6511e08761fdf4665b646ae1e6ba7e2fe8c8801
GODGEARHD <godgearhd22@gmail.com>
2022-04-06 14:26:42 +0200

    PROYECTO TFG DAM

--- script/calc.js
+++ script/calc.js
@@ -1,37 +1,299 @@
-var total = 0;
-
 function calc() {
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var so = document.getElementById('so');
-    var z = document.querySelector("#checkit");
-    var selectedOption = so.options[so.selectedIndex];
-    if ((selectedOption.value == "Windows 11 Home - 170.00€") || (selectedOption.value == "Windows 11 Pro - 280.00€")) {
-        check = 1;
-        z.checked = true;
-    } else {
-        if (check == 0) {
-            check = check;
-        } else {
-            check = 0;
-            z.checked = false;
-        }
+    var total = 0;
+    var procesador = document.getElementById('cpu');
+    var ventilador = document.getElementById('refri');
+    var memoria = document.getElementById('ram');
+    var placaBase = document.getElementById('placa');
+    var hdd1 = document.getElementById('disco1');
+    var hdd2 = document.getElementById('disco2');
+    var gabinete = document.getElementById('caja');
+    var fuente = document.getElementById('psu');
+    var grafica = document.getElementById('gpu');
+    var tarjeta = document.getElementById('audio');
+    var leer = document.getElementById('lector');
+    var cds = document.getElementById('dvd');
+    var pantalla = document.getElementById('monitor');
+    var teclas = document.getElementById('teclado');
+    var raton = document.getElementById('raton');
+    var altavoz = document.getElementById('altavoces');
+    var television = document.getElementById('tv');
+    var antena = document.getElementById('red');
+    var montaje = document.querySelector("#checkit");
+    var sistema = document.getElementById('so');
+    
+
+    /*//Select de Procesador
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
     }
+
+    //Select de Ventilador
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de Memoria
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de PlacaBase
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de HDD1
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de HDD2
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de Gabinete
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de Fuente
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de Gráfica
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de Tarjeta
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de Leer
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de CDS
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de Pantalla
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de Teclas
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de Raton
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de Altavoz
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de Television
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    //Select de Antena
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }*/
+
+    //CheckBox de Montaje
+    if (montaje.checked == true) {
+        total+=25;
+    } else if (montaje.checked == false) {
+        total+=0;
+    }
+
+    //Select de Sistema
+    var selectedSistema = sistema.options[sistema.selectedIndex];
+    if (selectedSistema.value == "Windows 10 Home - 145.00€") {
+        total+=145;
+    } else if (selectedSistema.value == "Windows 10 Pro - 259.00€") {
+        total+=259;
+    } else if (selectedSistema.value == "Windows 11 Home - 170.00€") {
+        total+=170;
+    } else if (selectedSistema.value == "Windows 11 Pro - 280.00€") {
+        total+=280;
+    } else if (selectedSistema.value == "-") {
+        total+=0;
+    }
+
+    document.getElementById("precio").innerHTML = total;
 }
