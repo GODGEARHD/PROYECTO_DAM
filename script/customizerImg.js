c28edcc10841184819de27e5df77cb72e57997ae
GODGEAR HD <godgearhd22@gmail.com>
2022-04-26 13:27:54 +0200

    PROYECTO TFG DAM

--- script/customizerImg.js
+++ script/customizerImg.js
@@ -71,6 +71,8 @@ function procesador() {
 function ventilador() {
     var refri = document.getElementById("refri");
     var selectedRefri = refri.options[refri.selectedIndex];
+    var cpu = document.getElementById("cpu");
+    var selectedCPU = cpu.options[cpu.selectedIndex];
     if (selectedRefri.value == "Asus ROG Ryujin 360") {
         document.getElementById("imgRefri").src = "../img/intel-core-i5-10600k.png";
         document.getElementById("imgRefri").style = "width: 120px;";
