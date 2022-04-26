1e33657a530bb2a4dad4000fac8fd7dead7327b9
GODGEAR HD <godgearhd22@gmail.com>
2022-04-26 14:12:46 +0200

    PROYECTO TFG DAM

--- script/customizerImg.js
+++ script/customizerImg.js
@@ -66,6 +66,24 @@ function procesador() {
         $("optgroup").siblings("#Intel").prop("disabled", false);
         $("optgroup").siblings("#AMD").prop("disabled", false);
     }
+    if (selectedRefri.value == "De Serie") {
+        if (selectedCPU.id == "Intel") {
+            document.getElementById("imgRefri").src = "../img/intel-serial-ref.png";
+            document.getElementById("imgRefri").style = "width: 120px;";
+        }
+        if (selectedCPU.id == "AMD-low") {
+            document.getElementById("imgRefri").src = "../img/amd-serial-ref-low.png";
+            document.getElementById("imgRefri").style = "width: 120px;";
+        }
+        if (selectedCPU.id == "AMD-high") {
+            document.getElementById("imgRefri").src = "../img/amd-serial-ref-high.png";
+            document.getElementById("imgRefri").style = "width: 120px;";
+        }
+        if (selectedCPU.value == "-") {
+            document.getElementById("imgRefri").src = "../img/seleccion.png";
+            document.getElementById("imgRefri").style = "width: 120px;";
+        }
+    }
 }
 
 function ventilador() {
