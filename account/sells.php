e64fb334c723be2ffefe7893c734ff73db4dbb3b
GODGEARHD <godgearhd22@gmail.com>
2022-05-03 23:28:02 +0200

    PROYECTO TFG DAM

--- account/sells.php
+++ account/sells.php
@@ -1,25 +1,23 @@
 <?php
 
-error_reporting(0);
+error_reporting(-1);
 
 require 'database.php';
 
 $message = '';
 
-if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['paypal']) && !empty($_POST['product'])) {
-  $sql = "INSERT INTO sells (name, email, paypal, product) VALUES (:name, :email, :paypal, :product)";
+if (!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['paypal']) && !empty($_POST['product'])) {
+  $sql = "INSERT INTO sells (name, email, paypal, product) VALUES (:nombre, :email, :paypal, :product)";
   $stmt = $conn->prepare($sql);
-  $stmt->bindParam(':name', $_POST['name']);
+  $stmt->bindParam(':nombre', $_POST['nombre']);
   $stmt->bindParam(':email', $_POST['email']);
   $stmt->bindParam(':paypal', $_POST['paypal']);
-  $stmt->bindParam(':product', $_REQUEST['product']);
+  $stmt->bindParam(':product', $_POST['product']);
 
   if ($stmt->execute()) {
-    $message = "Thanks for your purchase! We'll send you an email with the image you've just purchased";
+    $message = "¡Gracias por tu compra! Te enviaremos un e-mail en breve para que puedas confirmar tu pedido.";
   } else {
-    $message = 'Sorry, there must have been an issue while saving your comments';
-    echo "\nPDO::errorInfo():\n";
-    print_r($stmt->errorInfo());
+    $message = '¡Vaya! Parece que hemos tenido un problema al tramitar tu solicitud.';
   }
 }
 
